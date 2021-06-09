628ecbab49fb014fcde6febe7e17fd84384081ec
pgosar <pgosarsteam@gmail.com>
2021-06-08 20:51:11 -0500

    changed folder structure, added heapSort stuff

--- sorts/heapSort.py
+++ sorts/heapSort.py
@@ -1,1 +1,6 @@
 arr = [9,8,7,55,5,54,2,1,1]
+
+#heaps: tree based data structure - complete binary tree. A complete binary tree is one where
+#all but potentially the lowest level of the tree is completely filled
+
+ 
