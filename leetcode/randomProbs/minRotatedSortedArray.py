fbf31eec610bf94a4957bd9d053a77daa5dda34c
pgosar <pgosarsteam@gmail.com>
2021-08-31 09:56:07 -0500

    leetcode daily 8-31-2021

---
+++ leetcode/randomProbs/minRotatedSortedArray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def findMin(self, nums):
+        
+        start = 0
+        end = len(nums)-1
+        
+        while start < end:
+            
+            mid = (start + end)//2
+            
+            if nums[mid] > nums[end]:
+                
+                start = mid + 1
+            
+            else:
+                
+                end = mid
+        
+        return nums[start]
+        
+            
