778fff9290af09d7b67cb7d8cab5c300bb446cee
pgosar <pgosarsteam@gmail.com>
2021-12-23 18:46:15 -0600

    12/23/21

---
+++ leetcode/interview60/rmDuplicatesSortedListMedium.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode answer = new ListNode(0);
+        ListNode temp = answer;
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;
+                }
+            } else {
+                temp.next = head;
+                temp = temp.next;
+            }
+            head = head.next;
+        }
+        temp.next = null;
+        return answer.next;
+    }
+}
